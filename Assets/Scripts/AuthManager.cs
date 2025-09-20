203d200bc1bad1b09ef78cca1f4356a2e03e944c
koidxueqin <p21013129@student.newinti.edu.my>
2025-09-21 03:58:41 +0800

    Sign in anonymous test

---
+++ Assets/Scripts/AuthManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+
+public class AuthManager : MonoBehaviour
+{
+    public Text logTxt;
+
+    async void Start() {
+        await UnityServices.InitializeAsync();
+    }
+
+   public async void SignIn() {
+        await SignInA();
+    }
+
+
+   async Task SignInA() {
+
+        try
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            print("Sign in success!!");
+            print("Player id: " + AuthenticationService.Instance.PlayerId);
+            logTxt.text = "Player id: " + AuthenticationService.Instance.PlayerId;
+        }
+
+        catch (AuthenticationException ex) {
+            print("Sign in failed!!");
+            Debug.LogException(ex);
+        }
+        
+   }
+}
+
+    
